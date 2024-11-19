c04fb33c88ad10ff3353f2a2ec029c66e87d262c
FortWR <fixtien88@gmail.com>
2024-11-19 19:36:26 +0300

    пон

---
+++ _nuxt/ContentParagraph.BrcueMU4.js
@@ -0,0 +1,1 @@
+import{aj as n,aw as e,aE as o,aO as r}from"./layouts-default.BpgGATQN.js";const s=["innerHTML"],c=n({__name:"ContentParagraph",props:{content:{}},setup(t){const a=t;return(p,_)=>(e(),o("p",{class:"tp-content-paragraph",innerHTML:a.content.data.text},null,8,s))}}),l=r(c,[["__scopeId","data-v-6302db0d"]]);export{l as default};
